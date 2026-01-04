0c5f494731c45dde7decdccbcfa21879ebc2b652
wangqingkun <wqk17331@mail.haitian.com>
2026-01-04 15:27:59 +0800

    teat

---
+++ vite.config.ts
@@ -0,0 +1,58 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "@layui/unplugin-vue-components/vite";
+import { LayuiVueResolver } from "@layui/unplugin-vue-components/resolvers";
+import { resolve } from "path";
+
+const excludeComponents = ["LightIcon", "DarkIcon"];
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vite.dev/config/
+export default defineConfig(async () => ({
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "./src"),
+      },
+    ],
+  },
+  plugins: [
+    AutoImport({
+      resolvers: [LayuiVueResolver()],
+    }),
+    Components({
+      resolvers: [
+        LayuiVueResolver({
+          resolveIcons: true,
+          exclude: excludeComponents,
+        }),
+      ],
+    }),
+    vue(),
+  ],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent Vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
